0e1c44f75e01ff06a4580b3bc0916adb50130aa0
Vivian Munguía <andy_munguia@hotmail.com>
2022-07-17 20:44:54 -0700

    The classes dusuarios and CONEXIONMAESTRA were created

---
+++ Datos/dusuarios.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using usuarios369.Logica;
+
+namespace usuarios369.Datos
+{
+    class dusuarios
+    {
+        private SqlCommand cmd = new SqlCommand();
+        private int idusuario;
+        public bool insertar(lusuarios dt)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
