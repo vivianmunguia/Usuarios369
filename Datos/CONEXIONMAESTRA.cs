2f6b5711a9247250ed5f149022bac06c8141926a
Vivian Munguía <andy_munguia@hotmail.com>
2022-07-21 19:32:59 -0700

    Two methods were created to open and close the connection with the database

--- Datos/CONEXIONMAESTRA.cs
+++ Datos/CONEXIONMAESTRA.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Data;
+using System.Data.SqlClient;
 
 namespace usuarios369.Datos
 {
-    class CONEXIONMAESTRA
+    internal static class CONEXIONMAESTRA
     {
+        public static SqlConnection conexion = new SqlConnection
+            (@"Data source=LAPTOP-NLHO6AEK\SQLEXPRESS; Initial Catalog=USUARIOSDB; Integrated Security=true");
+
+        public static void abrir()
+        {
+            if (conexion.State == ConnectionState.Closed)
+            {
+                conexion.Open();
+            }
+        }
+
+        public static void cerrar()
+        {
+            if (conexion.State == ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+        }
     }
 }
